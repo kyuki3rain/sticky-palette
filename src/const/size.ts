a727e2a9cc5f5d71b54b05b4f9c911964f33f26a
kyuki3rain <kyuki3rain@gmail.com>
2023-03-20 18:13:34 +0000

    大きさを指定できるように変更

---
+++ src/const/size.ts
@@ -0,0 +1,12 @@
+export const SIZE_TAG = ['small', 'large'];
+
+export const getSize = (size_tag: string) => {
+	switch (size_tag) {
+		case 'small':
+			return { width: 256, height: 32, widthCSS: 'w-64', heightCSS: 'h-16' };
+		case 'large':
+			return { width: 256, height: 256, widthCSS: 'w-64', heightCSS: 'h-64' };
+		default:
+			return { width: 256, height: 32, widthCSS: 'w-64', heightCSS: 'h-16' };
+	}
+};
