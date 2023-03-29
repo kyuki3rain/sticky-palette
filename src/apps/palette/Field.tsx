6c6b86d3b132238a8ef217a54fdc993b64efbf93
kyuki3rain <kyuki3rain@gmail.com>
2023-03-29 01:39:38 +0000

    モーダルは選択できるようにする

--- src/apps/palette/Field.tsx
+++ src/apps/palette/Field.tsx
@@ -34,47 +34,49 @@ export default function Field() {
 	if (loading) return <Loading />;
 
 	return (
-		<div className="select-none">
-			<TransformWrapper
-				initialScale={1}
-				disabled={isMoveable}
-				minScale={0.1}
-				maxScale={3}
-				initialPositionX={width / 2}
-				initialPositionY={height / 2}
-				limitToBounds={false}
-				pinch={{ step: 5 }}
-				doubleClick={{ disabled: true }}
-				ref={ref}
-				onInit={() => {
-					setTransformRef(ref);
-				}}
-			>
-				{({ instance }) => (
-					<TransformComponent contentClass='main' wrapperStyle={{ height, width }}>
-						{[...fusenIds].map((id) => (
-							<Fusen
-								id={id}
-								key={id}
-								onDrag={onDrag}
-								onStop={onStop}
-								scale={instance.transformState.scale}
-							/>
-						))}
-					</TransformComponent>
-				)}
-			</TransformWrapper>
-			<button
-				className="absolute top-2 right-4 button block"
-				type="button"
-				onClick={() => supabase.auth.signOut()}
-			>
-				Sign Out
-			</button>
-			<div className='absolute top-0 -left-8 h-full overflow-y-scroll hidden-scrollbar hover:w-20'>
-				{COLOR_TAGS.map((color) => (
-					<ColorTag key={color} color={color} />
-				))}
+		<>
+			<div className="select-none">
+				<TransformWrapper
+					initialScale={1}
+					disabled={isMoveable}
+					minScale={0.1}
+					maxScale={3}
+					initialPositionX={width / 2}
+					initialPositionY={height / 2}
+					limitToBounds={false}
+					pinch={{ step: 5 }}
+					doubleClick={{ disabled: true }}
+					ref={ref}
+					onInit={() => {
+						setTransformRef(ref);
+					}}
+				>
+					{({ instance }) => (
+						<TransformComponent contentClass='main' wrapperStyle={{ height, width }}>
+							{[...fusenIds].map((id) => (
+								<Fusen
+									id={id}
+									key={id}
+									onDrag={onDrag}
+									onStop={onStop}
+									scale={instance.transformState.scale}
+								/>
+							))}
+						</TransformComponent>
+					)}
+				</TransformWrapper>
+				<button
+					className="absolute top-2 right-4 button block"
+					type="button"
+					onClick={() => supabase.auth.signOut()}
+				>
+					Sign Out
+				</button>
+				<div className='absolute top-0 -left-8 h-full overflow-y-scroll hidden-scrollbar hover:w-20'>
+					{COLOR_TAGS.map((color) => (
+						<ColorTag key={color} color={color} />
+					))}
+				</div>
 			</div>
 			<button
 				onClick={(e) => {
@@ -87,6 +89,6 @@ export default function Field() {
 				<AiOutlineUnorderedList />
 			</button>
 			<Modal />
-		</div>
+		</>
 	);
 }
